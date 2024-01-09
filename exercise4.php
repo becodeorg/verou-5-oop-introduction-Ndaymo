9daca4628ed82e898f3a927b8d4b7983ff6ae05f
Mohamed Ndayishimiye <ndayimodi@gmail.com>
2024-01-09 12:28:41 +0100

    Make all properties protected

--- exercise4.php
+++ exercise4.php
@@ -11,3 +11,25 @@ TODO: Make all the other prints work without error without changing the beverage
 
 USE TYPEHINTING EVERYWHERE!
 */
+
+require 'Beverage.php';
+
+class Beer extends Beverage
+{
+    protected $name;
+    protected $alcoholPercentage;
+
+    public function __construct(string $color, float $price, string $name, float $alcoholPercentage)
+    {
+        parent::__construct($color, $price);
+        $this->name = $name;
+        $this->alcoholPercentage = $alcoholPercentage;
+    }
+
+    function getAlcoholPercentage()
+    {
+        echo "The acohol percentage is $this->alcoholPercentage";
+
+    }
+
+}
