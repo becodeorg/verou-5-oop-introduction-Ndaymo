c9a08e7dd88eadf875ebff6a403b18d24c6ad555
Mohamed Ndayishimiye <ndayimodi@gmail.com>
2024-01-09 10:59:57 +0100

    Call the getAlcoholPercetage function in index.php

--- exercise2.php
+++ exercise2.php
@@ -21,15 +21,22 @@ USE TYPEHINTING EVERYWHERE!
 
 require 'Beverage.php';
 
-class beer extends beverage
+class Beer extends Beverage
 {
     public $name;
     public $alcoholPercentage;
 
     public function __construct(string $color, float $price, string $name, float $alcoholPercentage)
     {
+        parent::__construct($color, $price);
         $this->name = $name;
         $this->alcoholPercentage = $alcoholPercentage;
+    }
+
+    function getAlcoholPercentage()
+    {
+        echo "The acohol percentage is $this->alcoholPercentage";
+
     }
 
 }
--- index.php
+++ index.php
@@ -1,23 +1,12 @@
 <?php
 
 declare(strict_types=1);
-require 'Beverage.php';
-/* EXERCISE 1
-TODO: Create a class beverage.
-TODO: Create the properties color (string), price (float) and temperature (string) and also foresee a construct.
-TODO: Have a default value "cold" in the construct for temperature.
-
-Remember for now we will use properties and methods that can be accessed from everywhere.
-TODO: Make a getInfo function which returns "This beverage is <temperature> and <color>."
-TODO: Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold automatically
- print the getInfo on the screen.
-TODO: Print the temperature on the screen.
-
-USE TYPEHINTING EVERYWHERE!
-*/
-
-
-
+require 'exercise2.php';
 
 $cola = new Beverage('black', 2);
 $cola->getInfo();
+echo '<br>';
+$Duvel = new Beer('Blond', 3.5, 'Duvel', 8.5);
+$Duvel->getAlcoholPercentage();
+echo '<br>';
+$Duvel->getInfo();
