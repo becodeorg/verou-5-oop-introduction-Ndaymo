b1b51dd4d9e6058e638f29bb4c8a9a6b152f09fc
Mohamed Ndayishimiye <ndayimodi@gmail.com>
2024-01-09 11:20:17 +0100

    Add exercise 3 file

--- index.php
+++ index.php
@@ -10,3 +10,12 @@ $Duvel = new Beer('Blond', 3.5, 'Duvel', 8.5);
 $Duvel->getAlcoholPercentage();
 echo '<br>';
 $Duvel->getInfo();
+echo '<br>';
+$Duvel->getInfo();
+echo '<br>';
+echo 'The color of Duvel is ' . $Duvel->color;
+echo '<br>';
+echo 'The alcohol percentage of Duvel is ' . $Duvel->alcoholPercentage;
+echo '<br>';
+
+//This will result into the error $cola->getAlcoholPercentage();
